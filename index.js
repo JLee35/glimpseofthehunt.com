9c21810a0264c44ade573c314afddcff64d2ae68
jaredleedev <jaredlee.dev@gmail.com>
2019-10-19 15:26:02 -0500

    Adds new banner and navbar.

---
+++ index.js
@@ -0,0 +1,12 @@
+var prev = 0;
+var $window = $(window);
+var nav = $('.scrollhide-nav');
+
+$window.on('scroll', function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
+
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
+
