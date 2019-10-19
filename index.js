7bafd96b167490b8daf465efbebdad018dd5c8e4
jaredleedev <jaredlee.dev@gmail.com>
2019-10-19 16:13:54 -0500

    Adds dynamic image slider.

--- index.js
+++ index.js
@@ -10,3 +10,44 @@ $window.on('scroll', function(){
 
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
 
+
+
+// Globals
+var sliderImages = [
+    "img/newShowcase/deer-1-right-face.jp2",
+    "img/newShowcase/deer2-straight.jp2",
+    "img/newShowcase/deer-3-shoulder.jp2",
+    "img/newShowcase/eian-with-kill.jp2",
+    "img/newShowcase/eian-right-face.jp2",
+    "img/newShowcase/eian-straight.jp2",
+    "img/newShowcase/eian-eye.jp2",
+    "img/newShowcase/bradenBull.jp2",
+    "img/newShowcase/elk-whole.jp2",
+    "img/newShowcase/elk-nose.jp2",
+    "img/newShowcase/elk-vein.jp2"
+];
+
+var sliderElement = $(".orbit-container");
+
+function populateSlider() {
+    sliderImages.forEach(function (elem) {
+        sliderElement.append(createOrbitSlideElement(elem));
+    });
+}
+
+function createOrbitSlideElement(src) {
+    var newListItem = document.createElement("li");
+    newListItem.classList.add("orbit-slide");
+
+    var itemImg = document.createElement("img");
+    itemImg.classList.add("orbit-image");
+    itemImg.src = src;
+
+    newListItem.appendChild(itemImg);
+
+    return newListItem;
+}
+
+$(document).ready(function() {
+     populateSlider();
+});
