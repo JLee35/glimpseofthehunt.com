89f75473aa01addfb6f4a700663594cec6ce9c61
jaredleedev <jaredlee.dev@gmail.com>
2019-10-21 13:56:36 -0500

    Replaces old slider.

--- index.js
+++ index.js
@@ -25,26 +25,67 @@ var sliderImages = [
     "img/newShowcase/elk-vein.jp2"
 ];
 
-var sliderElement = $(".orbit-container");
+var sliderElement = $(".slideshow-container");
 
 function populateSlider() {
-    sliderImages.forEach(function (elem) {
-        sliderElement.append(createOrbitSlideElement(elem));
+    // Update coverSlideCount
+    $("#coverSlideCount").text("1/" + sliderImages.length);
+    sliderImages.forEach(function (elem, index) {
+        sliderElement.append(createSlideShowElement(elem, index));
     });
 }
 
-function createOrbitSlideElement(src) {
-    var newListItem = document.createElement("li");
-    newListItem.classList.add("orbit-slide");
+function createSlideShowElement(src, index) {
+    index+=2; // There is a cover image by default, and we are using 0 based indexing.
+    var newSlideDiv = document.createElement("div");
+    newSlideDiv.classList.add("mySlides", "fade");
+
+    var numberText = document.createElement("div");
+    numberText.classList.add("numbertext");
+    numberText.innerText = index + '/' + sliderImages.length; // Example: "1/3"
 
     var itemImg = document.createElement("img");
-    itemImg.classList.add("orbit-image");
+    itemImg.style.width = "100%";
     itemImg.src = src;
 
-    newListItem.appendChild(itemImg);
+    newSlideDiv.appendChild(numberText);
+    newSlideDiv.appendChild(itemImg);
 
-    return newListItem;
+    return newSlideDiv;
 }
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
 
 $(document).ready(function() {
      populateSlider();
